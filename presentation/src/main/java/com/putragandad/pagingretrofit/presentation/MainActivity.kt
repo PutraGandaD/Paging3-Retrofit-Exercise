5bc9e8e6bf61e6cd7cd12c23d5cf7b12cd13ca1d
PutraGandaD <putraganda.dewata@gmail.com>
2025-09-24 22:00:50 +0700

    Migrate UI completely to Jetpack Compose

--- presentation/src/main/java/com/putragandad/pagingretrofit/presentation/MainActivity.kt
+++ presentation/src/main/java/com/putragandad/pagingretrofit/presentation/MainActivity.kt
@@ -1,72 +1,55 @@
 package com.putragandad.pagingretrofit.presentation
 
+import android.os.Build
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doAfterTextChanged
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.paging.PagingData
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.putragandad.pagingretrofit.presentation.databinding.ActivityMainBinding
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.putragandad.pagingretrofit.presentation.theme.JetpackComposePagingRetrofitTheme
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
-    private val mainViewModel : MainViewModel by inject()
-    private val repoAdapter = RepoAdapter()
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            MyApp()
         }
 
-        setSupportActionBar(findViewById(R.id.my_toolbar))
-        setUpRecyclerView()
-        observer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+    }
+}
 
-        binding.etSearchText.doAfterTextChanged { inputText ->
-            lifecycleScope.launch {
-                delay(3000L)
-                mainViewModel.searchRepos(inputText.toString())
+@Composable
+fun MyApp() {
+    JetpackComposePagingRetrofitTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { contentPadding ->
+            Surface(
+                modifier = Modifier
+                    .padding(contentPadding)
+            ) {
+                Text("Hello")
             }
         }
     }
+}
 
-    private fun observer() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                mainViewModel.searchResultState.collectLatest { state ->
-                    repoAdapter.submitData(state)
-                }
-            }
-        }
-    }
-
-    private fun setUpRecyclerView() {
-        binding.list.adapter = repoAdapter
-        binding.list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        binding.list.adapter = repoAdapter.withLoadStateHeaderAndFooter(
-            header = RepoItemLoadingStateAdapter { repoAdapter.retry() },
-            footer = RepoItemLoadingStateAdapter { repoAdapter.retry() }
-        )
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MyAppPreview() {
+    JetpackComposePagingRetrofitTheme {
+        MyApp()
     }
 }
